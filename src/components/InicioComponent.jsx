a96f33a815aee08bf91ed8ba4664e8b74e1283cc
Anthony <llanestony6@gmail.com>
2025-05-13 00:08:58 -0600

    Configurar login como página principal y actualizar redirecciones

--- src/components/InicioComponent.jsx
+++ src/components/InicioComponent.jsx
@@ -6,7 +6,7 @@ const InicioComponent = () => {
     <div className="container">
       <div className="row">
         <div>
-          <hr />
+          
         </div>
         <div className="col-md-12">
           <h1 className="text-center">Bienvenido al centro de administración</h1>
@@ -15,7 +15,7 @@ const InicioComponent = () => {
       </div>
       <div className="row">
         <div className="col-md-12">
-          <p className="text-center">Aquí puedes agregar, editar y eliminar registros de empleados, clientes y productos.</p>
+          <p className="text-center"></p>
         </div>
       </div>
     </div>
